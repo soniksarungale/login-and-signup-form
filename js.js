336f66604c326241d0a26e5bc748b6826132b110
sonik sarungale <sonik.sarungale@gmail.com>
2019-05-10 14:44:54 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+function show_create(){
+    $(".login-form").css("display","none");
+    $(".signup-form").fadeIn();
+}
+function show_login(){
+    $(".signup-form").css("display","none");
+    $(".login-form").fadeIn();
+}
